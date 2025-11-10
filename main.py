2fc9f59d13ca5327de08c10e7a6f53b482592ff4
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2025-11-10 21:13:40 +0200

    all tasks

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from src.seed import fillGroup, clearAllData, fillStudent, fillTeacher, fillDiscipline, fillGrades
-from src.my_select import task1, task2, task3,task4,task5,task6
+from src.my_select import select_1, select_2, select_3, select_4, select_5, select_6, select_7, select_8,select_9,select_10
 
 
 def main():
-    # task1()
-    # task2(1)
-    # task3(4)
-    # task4()
-    # task5(3)
-    task6(2)
+    # select_1()
+    # select_2(1)
+    # select_3(4)
+    # select_4()
+    # select_5(3)
+    # select_6(2)
+    # select_7(1,1)
+    # select_8(4)
+    # select_9(1)
+    # select_10(1,2)
 
     # clearAllData()
     # fillGroup(3)
@@ -17,20 +21,20 @@ def main():
     # fillDiscipline()
     # fillGrades()
 
-    # while True:
-    #     print("Меню:")
-    #     print("1. Заповнити db")
-    #     print("2. Вийти з додатку")
+    while True:
+        print("Меню:")
+        print("1. Заповнити db")
+        print("2. Вийти з додатку")
 
-    #     choice = int(input("Оберіть дію 1-2: "))
+        choice = int(input("Оберіть дію 1-2: "))
 
-    #     if choice == 1:
-    #         fill_db()
-    #         print("Заповнено!")
+        if choice == 1:
+            fill_db()
+            print("Заповнено!")
 
-    #     elif choice == 2:
-    #         print("До побачення!")
-    #         break
+        elif choice == 2:
+            print("До побачення!")
+            break
 
 
 if __name__ == "__main__":
--- src/my_select.py
+++ src/my_select.py
@@ -4,7 +4,7 @@ from .models import Student, Teacher, Grade, Group, Discipline
 from .configuration import engine
 
 
-def task1():
+def select_1():
     with Session(engine) as session:
         try:
             top_students = (
@@ -29,7 +29,7 @@ def task1():
             session.commit()
 
 
-def task2(discipline):
+def select_2(discipline):
     with Session(engine) as session:
         try:
             top_students = (
@@ -55,7 +55,7 @@ def task2(discipline):
             session.commit()
 
 
-def task3(discipline):
+def select_3(discipline):
     with Session(engine) as session:
         try:
             group_avg_grades = (
@@ -63,14 +63,14 @@ def task3(discipline):
                     Group.group_name,
                     func.avg(Grade.grade).label("average_grade")
                 )
-                .join(Group.students)       
+                .join(Group.students)
                 .join(Student.grades)
                 .filter(Grade.discipline_id == discipline)
                 .group_by(Group.id)
                 .all()
             )
 
-            for group,avg_grade in group_avg_grades:
+            for group, avg_grade in group_avg_grades:
                 print(f"{group}: {avg_grade:.2f}")
 
         except:
@@ -80,7 +80,7 @@ def task3(discipline):
             session.commit()
 
 
-def task4():
+def select_4():
     with Session(engine) as session:
         try:
             group_avg = (
@@ -100,7 +100,7 @@ def task4():
             session.commit()
 
 
-def task5(teacher):
+def select_5(teacher):
     with Session(engine) as session:
         try:
             disciplines = (
@@ -121,13 +121,13 @@ def task5(teacher):
             session.commit()
 
 
-def task6(group):
+def select_6(group):
     with Session(engine) as session:
         try:
             disciplines = (
                 session.query(
                     Student.name
-                ).filter(Student.group_id==group)
+                ).filter(Student.group_id == group)
                 .all()
             )
 
@@ -141,19 +141,97 @@ def task6(group):
             session.commit()
 
 
-def task7(group,discipline):
+def select_7(group, discipline):
     with Session(engine) as session:
         try:
-            disciplines = (
+            grades = (
                 session.query(
-                    Discipline.discipline_name
+                    Student.name, Grade.grade
                 )
-                .select_from(Discipline)
-                .filter(Discipline.teacher_id == teacher).all()
+                .join(Student.grades)
+                .filter(Student.group_id == group)
+                .filter(Grade.discipline_id == discipline)
+                .all()
             )
 
-            for dis in disciplines:
-                print(f"{dis}")
+            for stud, grade in grades:
+                print(f"{stud}: {grade}")
+
+        except:
+            session.rollback()
+            raise
+        else:
+            session.commit()
+
+
+def select_8(teacher):
+    with Session(engine) as session:
+        try:
+
+            avg_grade = (
+                session.query(func.avg(Grade.grade))
+                .join(Grade.discipline)
+                .filter(Discipline.teacher_id == teacher)
+                .scalar()
+            )
+
+            teacher_name = session.get(Teacher, teacher)
+
+            print(
+                f"Середній бал, який ставить викладач {teacher_name.name}: {avg_grade}")
+        except:
+            session.rollback()
+            raise
+        else:
+            session.commit()
+
+
+def select_9(student):
+    with Session(engine) as session:
+        try:
+
+            courses = (
+                session.query(Discipline.discipline_name)
+                .join(Grade)
+                .filter(Grade.student_id == student)
+                .distinct()
+                .all()
+            )
+
+            student_name = session.get(Student, student)
+
+            print(f"Студент {student_name.name} відвідує курси:")
+            for (course_name,) in courses:
+                print(f"- {course_name}")
+
+        except:
+            session.rollback()
+            raise
+        else:
+            session.commit()
+
+
+def select_10(student,teacher):
+    with Session(engine) as session:
+        try:
+
+            courses = (
+                session.query(Discipline.discipline_name)
+                .join(Grade)
+                .filter(Grade.student_id == student)
+                .filter(Discipline.teacher_id == teacher)
+                .distinct()
+                .all()
+            )
+
+            student_name = session.get(Student, student)
+            teacher_name = session.get(Teacher, teacher)
+
+            print(
+                f"Курси, які студент {student_name.name} слухає у викладача {teacher_name.name}:")
+            
+            for (course_name,) in courses:
+                print(f"- {course_name}")
 
         except:
             session.rollback()
